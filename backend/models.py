1d31456ab991e3109b32952d00d938826102776e
Антощенко Артем Олегович <279017030@ACADEMY.TOP>
2025-11-26 20:40:57 +0300

    create project

---
+++ backend/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+
+class TodoStatus(str, Enum):
+    PENDING='pending'
+    IN_PROGRESS='in_progress'
+    COMPLETED='completed'
+
+class TodoBase(BaseModel):
+    title:str = Field(..., min_length=1, max_length=200, description='Название задачи')
+    description:Optional[str]=Field(None, max_length=1000,  description='Описание задачи')
+    status:TodoStatus=Field(default=TodoStatus.PENDING,  description='Статус задачи')
+
+class TodoCreate(TodoBase):
+    pass
+
+class TodoUpdate(BaseModel):
+    title:Optional[str]=Field(None, min_length=1, max_length=200)
+    description:Optional[str]=Field(None, max_length=1000)
+    status:Optional[TodoStatus]=Field(None)
+
+class TodoInDB(TodoBase):
+    id:int
+    created_at:datetime
+    updatet_at:datetime
+    class Config:
+        from_attributes=True
