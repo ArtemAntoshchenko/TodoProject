b3d1def36b435601d5842c3be44aac2237fe10b5
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-12-08 21:21:41 +0300

    update config.py

---
+++ backend/app/core/config.py
@@ -0,0 +1,25 @@
+import os
+from typing import List, Optional
+from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn, validator
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..','..','.env'))
+class Settings(BaseSettings):
+    PROJECT_NAME:str='Task Manager'
+    VERSION:str ='1.0.0'
+    API_V1_STR:str='/api/v1'
+    DEBUG:bool=False
+
+    POSTGRES_USER:str
+    POSTGRES_PASSWORD:str
+    POSTGRES_HOST:str='localhost'
+    POSTGRES_PORT:str='5432'
+    POSTGRES_DB:str
+
+    DATABASE_URL:Optional[PostgresDsn]=None
+    @validator('DATABASE_URL', pre=True)
+    def assemble_db_connection(cls, v:Optional[str], values):
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build()
